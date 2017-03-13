bbf6bafd482e122932250013756abe486344dbc1
444Duarte <duartemr.pinto@gmail.com>
2017-03-13 11:15:47 +0000

    Initial commit

---
+++ src/openmp-ex.cpp
@@ -0,0 +1,15 @@
+#include <omp.h>
+#include <stdio.h>
+#include <iostream>
+#include <iomanip>
+#include <time.h>
+#include <cstdlib>
+//#include <papi.h>
+
+using namespace std;
+
+#define SYSTEMTIME clock_t 
+
+int main (int argc, char *argv[]){
+
+}
